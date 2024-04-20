2906dae4b4dc216ac5154e7932422f8ae14db7b2
shersheryar <p218027@pwr.nu.edu.pk>
2024-04-20 19:37:03 +0500

    displaying critic score

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -6,12 +6,14 @@ export interface Platform {
    id: number;
    name:string;
    slug: string;
+
   }
 export interface Game {
     id: number;
     name: string;
     background_image: string;
     parent_platforms : {platform:Platform}[];
+    metacritic: number;
   }
   interface FetchGamesResponse {
     count: number;
